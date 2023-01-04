aa8c2913eff970cb7c6a46183607303eef6e8c54
milon285 <121970402+milon285@users.noreply.github.com>
2023-01-04 13:43:40 -0500

    paperbox
    my web ap

---
+++ paperbox/box/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def Home(request):
+    return render( request,'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def services(request):
+    return render(request, 'services.html')
